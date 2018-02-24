76980eedbc5ce31e903f53af0c32ac827d5326c1
mista-j <johnnie.a.hernandez@gmail.com>
2018-02-24 14:48:09 -0500

    added junit test

---
+++ jUnitTestExample/src/assg5_Hernandez/ComplexNumTest.java
@@ -0,0 +1,72 @@
+package assg5_Hernandez;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ComplexNumTest {
+
+	private ComplexNum l = new ComplexNum();
+	private ComplexNum m = new ComplexNum();
+	private ComplexNum n = new ComplexNum();
+	@Test
+	public void testComplexNum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComplexNumDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetReal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetImaginary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetReal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetImaginary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSub() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMul() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNeg() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}
