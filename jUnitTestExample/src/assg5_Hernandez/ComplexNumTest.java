1fd7af79f58bff424f1ec85a02a657921e8b9762
mista-j <johnnie.a.hernandez@gmail.com>
2018-02-24 14:54:23 -0500

    first try at junit

--- jUnitTestExample/src/assg5_Hernandez/ComplexNumTest.java
+++ jUnitTestExample/src/assg5_Hernandez/ComplexNumTest.java
@@ -6,27 +6,34 @@ import org.junit.Test;
 
 public class ComplexNumTest {
 
-	private ComplexNum l = new ComplexNum();
-	private ComplexNum m = new ComplexNum();
-	private ComplexNum n = new ComplexNum();
+	private ComplexNum l;
+	private ComplexNum m;
+	private ComplexNum n;
 	@Test
 	public void testComplexNum() {
-		fail("Not yet implemented");
+		l = new ComplexNum();
 	}
 
 	@Test
 	public void testComplexNumDoubleDouble() {
-		fail("Not yet implemented");
+		m = new ComplexNum(4.6, 10.2);
+		n = new ComplexNum(2.3, 5.1);
+		
 	}
 
 	@Test
 	public void testGetReal() {
-		fail("Not yet implemented");
+		assertEquals("Default should be 0.0", 0.0, l.getReal(), 0.0);
+		assertEquals("Default should be 4.6", 4.6, m.getReal(), 0.0);
+		assertEquals("Default should be 2.3", 2.3, n.getReal(), 0.0);
 	}
 
 	@Test
 	public void testGetImaginary() {
-		fail("Not yet implemented");
+		assertEquals("Default should be 0.0", 0.0, l.getImaginary(), 0.0);
+		assertEquals("Default should be 10.2", 10.2, m.getImaginary(), 0.0);
+		assertEquals("Default should be 5.1", 5.1, n.getImaginary(), 0.0);
+
 	}
 
 	@Test
