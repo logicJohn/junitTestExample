b966793ac3294b9b9f9a8ee1bf5d153469a3a829
mista-j <johnnie.a.hernandez@gmail.com>
2018-02-27 00:28:08 -0500

    hmmmm....

--- jUnitTestExample/src/assg5_Hernandez/ComplexNumTest.java
+++ jUnitTestExample/src/assg5_Hernandez/ComplexNumTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class ComplexNumTest {
+public class ComplexNumTest extends ComplexNum{
 
 	private double a = 4.6;
 	private double b = 10.2;
